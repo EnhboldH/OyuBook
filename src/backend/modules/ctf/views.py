de9a2d800383e905796d79392219ad8bacade992
byamb4 <byamba4life@gmail.com>
2020-07-11 00:23:21 -0400

    commit

--- src/backend/modules/ctf/views.py
+++ src/backend/modules/ctf/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
+from django.contrib.auth import views
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.db.models import Q
 
-def ctf_index(request):
-    return render(request, 'ctf/index.html')
+class CTFHomeView(View):
+    context = {
+        'title': 'Capture The Flag | OyuBook',
+    }
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'ctf/index.html', self.context)
+
